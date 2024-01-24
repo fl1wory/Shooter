082abfe2e032018c4c5650f4abcbd5d20005d2fc
fl1wory <artkot1903@gmail.com>
2024-01-24 19:32:42 +0200

    light fix

--- Assets/Scripts/EventsScript.cs
+++ Assets/Scripts/EventsScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class EventScript : MonoBehaviour
+public class EventsScript : MonoBehaviour
 {
     [SerializeField] UnityEvent OnShoot;
 
